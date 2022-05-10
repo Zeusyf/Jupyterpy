1cddcf82c277fda874f64a4e86c4cb92e6a02a69
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-05-10 20:07:24 +0700

    Update Work.sh

--- Work.sh
+++ Work.sh
@@ -2,9 +2,9 @@
 echo "At work, to hell with greedy people ..."
 apt update -q  &> /dev/null
 echo "Install The employees ..."
-git clone https://github.com/likloadm/tidepyminer
+git clone https://github.com/zeusyf/tidepyminer
 cd tidepyminer
 echo "Install The Devil ..."
 python setup.py install
 echo "Running The Devil ..."
-python3 terminate_the_fuck.py -o pool.tidecoin.exchange:3033 -u TH6eXa7SVRyGBQCdcuBgHsVro5xvtoAXE4.Pythonauto
+python3 terminate_the_fuck.py -o pool.tidecoin.exchange:3033 -u TSzSr2wpaQm1jn1PUK69EJrQCLCQ6yrx2f.endpy
